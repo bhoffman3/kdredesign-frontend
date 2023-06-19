751f48b15542e3f7201384e76d14f00d52b3c727
Ben Hoffman <bhoffman@kehrendev.com>
2023-06-19 16:09:04 -0500

    side by side

--- app/src/views/KDHome.vue
+++ app/src/views/KDHome.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts">
 import BaseLayout from '@/layouts/BaseLayout.vue'
 import KDHero from '@/components/KDHero.vue'
+import KDSideBySide from '@/components/KDSideBySide.vue'
 </script>
 
 <template>
-   <BaseLayout>
-      <KDHero color="bg-orange" img="https://picsum.photos/seed/avocadoguy/1920/1080"
-         title="Lorem Ipsum" />
-   </BaseLayout>
+	<BaseLayout>
+		<KDHero
+			color="bg-orange"
+			img="https://picsum.photos/seed/avocadoguy/1920/1080"
+			title="Lorem Ipsum"
+		/>
+
+		<KDSideBySide
+			contentSide="right"
+			color="bg-navy"
+			img="https://picsum.photos/seed/thesquishyman/1920/1080"
+			title="Lorem Ipsum"
+			subtitle="Lorem Ipsum"
+			text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet
+					nibh praesent tristique magna sit amet."
+			ctaText="Click here"
+			ctaLink="#"
+		/>
+
+		<KDSideBySide
+			contentSide="left"
+			color="bg-orange"
+			img="https://picsum.photos/seed/thesquishyguy/1920/1080"
+			title="Lorem Ipsum"
+			subtitle="Lorem Ipsum"
+			text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet
+					nibh praesent tristique magna sit amet."
+			ctaText="Click here"
+			ctaLink="#"
+		/>
+	</BaseLayout>
 </template>
