866aec2547a5e91f8980527962100599834f38f1
Ben Hoffman <bhoffman@kehrendev.com>
2023-06-30 13:46:54 -0500

    even more hero changes

--- app/src/components/KDHero/KDHero.vue
+++ app/src/components/KDHero/KDHero.vue
@@ -13,7 +13,7 @@ const props = defineProps({
 
 <style lang="scss">
 .kmask {
-	clip-path: polygon(55% 0%, 100% 0%, 100% 100%, 55% 100%);
+	clip-path: polygon(0% 55%, 100% 55%, 100% 100%, 0% 100%);
 
 	@screen md {
 		clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%);
@@ -22,51 +22,64 @@ const props = defineProps({
 </style>
 
 <template>
-	<div class="kd-hero overflow-hidden">
+	<div class="kd-hero overflow-hidden relative" :class="color">
 		<div
-			class="w-full h-[400px] md:h-[350px] lg:h-[400px] xl:h-[500px] py-2 relative"
-			:class="color"
+			class="kd-hero__in w-full h-[400px] md:h-[350px] lg:h-[400px] xl:h-[500px]"
 		>
-			<div class="flex justify-end w-full h-full absolute top-0 left-0">
-				<!-- mask -->
-				<div class="w-full md:w-[60%] lg:w-[50%] h-full rotate-90 md:rotate-0">
-					<div class="w-full h-full kmask" :class="color">
-						<KDSquareSplashV2
-							componentBackgroundColor="orange"
-							:squareFinalOpacity="0.75"
-						/>
-
-						<div
-							class="w-full h-full flex flex-col justify-center align-middle absolute top-0"
-						>
-							<div class="flex justify-center pr-16 bg-navy py-8">
-								<div>
-									<ComponentTitle :text="title" :color="'text-white'" />
-									<div class="bg-white rounded w-full h-[3px] mb-6"></div>
-									<KDButton text="Click here" url="#" />
-								</div>
-							</div>
-						</div>
-					</div>
+			<div
+				class="kd-hero__content w-full md:w-2/3 xl:w-1/2 h-full float-none md:float-right relative z-10"
+			>
+				<div class="kmask w-full h-full" :class="color">
+					<KDSquareSplashV2
+						componentBackgroundColor="orange"
+						:squareFinalOpacity="0.25"
+					/>
 				</div>
-				<!-- /mask -->
 
-				<!-- content -->
-				<!-- <div
-					class="content absolute w-full md:w-[45%] h-full flex justify-center items-end md:items-center"
+				<div
+					class="w-full md:w-[80%] h-1/2 md:h-full flex justify-center items-center absolute bottom-0 md:top-0 right-0"
 				>
-					<div
-						class="w-full flex flex-col flex-wrap justify-center items-center shadow bg-navy"
-					>
+					<div class="flex justify-center flex-wrap">
 						<ComponentTitle :text="title" :color="'text-white'" />
 						<div class="bg-white rounded w-full h-[3px] mb-6"></div>
 						<KDButton text="Click here" url="#" />
 					</div>
-				</div> -->
-				<!-- /content -->
+				</div>
 			</div>
 
-			<img :src="img" alt="kd hero" class="w-full h-full object-cover" />
+			<div class="kd-hero__img w-full h-full absolute top-0 left-0 py-2">
+				<img :src="img" alt="kd hero" class="w-full h-full object-cover z-0" />
+			</div>
 		</div>
 	</div>
 </template>
+
+<!-- <template>
+	<div class="kd-hero overflow-hidden relative">
+		<div
+			class="w-full h-[400px] md:h-[350px] lg:h-[400px] xl:h-[500px] flex justify-end py-2"
+			:class="color"
+		>
+			<div class="w-full md:w-[60%] lg:w-[50%] h-full relative z-10">
+				<div class="kmask w-full h-full" :class="color">
+					<KDSquareSplashV2
+						componentBackgroundColor="orange"
+						:squareFinalOpacity="0.75"
+					/>
+				</div>
+
+				<div class="content w-full h-full bg-red-500">
+					<ComponentTitle :text="title" :color="'text-white'" />
+					<div class="bg-white rounded w-full h-[3px] mb-6"></div>
+					<KDButton text="Click here" url="#" />
+				</div>
+			</div>
+
+			<img
+				:src="img"
+				alt="kd hero"
+				class="w-full h-full object-cover absolute top-0 left-0 z-0"
+			/>
+		</div>
+	</div>
+</template> -->
